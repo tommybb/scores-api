40ad6c60ec2631ec3c3a8561ca857227476a6f00
tommybb <tdudzik@gmail.com>
2014-09-14 21:04:11 +0200

    refactor views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :results
+
+  def to_label
+    "#{last_name} #{name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
   resources :competitions
-
   resources :results
-
   resources :users
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'results#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
