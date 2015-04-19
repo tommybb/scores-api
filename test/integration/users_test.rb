9e9caa32399da99e44c9d6598bc0e28a4f8fb93c
tommybb <tdudzik@gmail.com>
2015-04-19 19:17:11 +0200

    delete minitests

--- test/integration/users_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UsersTest < ActionDispatch::IntegrationTest
-  test "Anyone can view user information" do
-    user = FactoryGirl.create(:user_kazimierz)
-    get user_url(user)
-    assert_response :success
-  end
-end
